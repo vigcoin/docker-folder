0a8ebf0f52b539b458842bd78abac92d87e996e6
calidion <calidion@gmail.com>
2019-01-07 22:16:58 +0800

    added www/config

---
+++ www/config/config.js
@@ -0,0 +1,32 @@
+var DOMAIN = "localhost";
+var email = "calidion@gmail.com";
+
+
+var api = "http://" + DOMAIN + ":8119";
+
+var api_blockexplorer = "http://" + DOMAIN + ":19810";
+
+var poolHost = DOMAIN;
+
+var irc = "irc.freenode.net/#vig";
+
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "/?hash={id}#blockchain_block";
+
+var transactionExplorer = "/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "vig": [
+        ["vig.tyk.im", "http://vig-pool.tyk.im:8119"],
+        ["pool.vigcoin.org", "pool.vigcoin.org:8119"],
+    ],
+    "bcn": [
+        ["democats.org", "http://pool.democats.org:7603"]
+    ]
+};
